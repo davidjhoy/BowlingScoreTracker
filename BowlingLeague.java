c9d3b7f2a6a2d615d97aabf150e51bebef34b7eb
david <davidjhoyjr@gmail.com>
2023-02-28 18:54:33 -0500

    basic setup

---
+++ BowlingLeague.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class BowlingLeague {
+    //A bowler has a name and a list of games that they have played
+    //Bowler might have a total score count, number of strikes, average score per game
+    // A game will have 
+
+    
+    ArrayList<Bowler> roster;
+    Games[] games;
+    
+
+    public void BowlingLeague(){
+        System.out.println("Initiliazing League");
+        roster = new ArrayList<Bowler>();
+    }
+
+    public void toString(){
+        System.out.println("Output all the league info here");
+    }
+
+    public void addGame(){
+
+    }
+
+    // public void 
+
+    public static void main(String[] args){
+        //implement BowlingLeague instance
+    }
+}
---
+++ Game.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class Game {
+    private int score;
+    private Date date;
+    public Bowler[] players;
+
+
+    public void Game(){
+        
+    }
+
+
+}
